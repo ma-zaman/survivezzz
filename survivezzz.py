77d6b64d68520c4b608dc1242a6517619af5ea0b
theJOKER26 <zamanmuhammadali@gmail.com>
2017-06-04 22:31:38 +0200

    add pictures, change zombies movement, add GameOver

--- survivezzz.py
+++ survivezzz.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 import pygame
 import random
+import tkMessageBox
 
 pygame.mixer.init()
 
@@ -22,25 +23,24 @@ zombie3.set_volume(0.5)
 
 root = Tk()
 
+def new():
+    c.delete(ALL)
+
 def keypress(event):
     global x,y,x1,y1,player
     key = event.keysym
-    if key == 'Up':
+    if key == 'Up' and y>5:
         y -= 10
         y1 -= 10
-    elif key ==  'Down':
+    elif key ==  'Down' and y1<H/2:
         y += 10
         y1 += 10
-    elif key == 'Left':
+    elif key == 'Left' and x>5:
         x -= 10
         x1 -= 10
-    elif key == 'Right':
+    elif key == 'Right' and x1<W/2:
         x+=10
         x1 += 10
-    z0.movement()
-    z1.movement()
-    z0.kill_check()
-    z1.kill_check()
     c.coords(player,x,y,x1,y1)
 
 class zombie():
@@ -64,17 +64,38 @@ class zombie():
 
     def kill_check(self):
         global player_life
-        if x+2 <  self.x and x1-2 > self.x and y+2 < self.y and y1-2 > self.y:
+        if x-2 <  self.x and x1+2 > self.x and y-2 < self.y and y1+2 > self.y:
             player_life -= 1
+            life()
             print(player_life)
+
+def zombie_main():
+    z0.movement()
+    z1.movement()
+    z0.kill_check()
+    z1.kill_check()
+    root.after(100,zombie_main)
+
+def life():
+    global player_life
+    if player_life == 0:
+        tkMessageBox.showwarning('SURVIVEZZZ','!!!!!GAME OVER !!!!!')
+        root.destroy()
+
 
 W = root.winfo_screenwidth()
 H = root.winfo_screenheight()
 
 c = Canvas(root,width= W/2,height=H/2,bg='grey')
+
+picbackground = PhotoImage(file="pictures/background.gif")
+picheart = PhotoImage (file="pictures/heart.gif")
+picdead = PhotoImage (file="pictures/dead_heart.gif")
+c.create_image(0,0,anchor=NW, image=picbackground)
+
 x = W/100
 y = H/100
-player_life = 3
+player_life = 5
 
 if x <=y:
     diff = x
@@ -83,10 +104,13 @@ else:
 
 x=y=10-diff
 x1=y1=10+diff
+life()
 
 player = c.create_oval(x,y,x1,y1,outline = 'black', fill = 'firebrick')
 z0 = zombie(diff/2, 3)
 z1 = zombie(diff/3,6)
+
+zombie_main()
 
 c.bind('<Key>',keypress)
 c.focus_set()
